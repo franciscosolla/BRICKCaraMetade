0d8efae3c3e1adf9179e6442421d30de71a220f1
Lucasjuv <lucasjuviniano@gmail.com>
2015-03-24 13:35:17 -0300

    Camera implemented

--- CaraMetade/CaraMetade/ViewController.h
+++ CaraMetade/CaraMetade/ViewController.h
@@ -7,9 +7,9 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MobileCoreServices/MobileCoreServices.h>
 
-@interface ViewController : UIViewController
-
+@interface ViewController : UIViewController 
 
 @end
 
--- CaraMetade/CaraMetade/ViewController.m
+++ CaraMetade/CaraMetade/ViewController.m
@@ -8,9 +8,11 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 
-@property (weak, nonatomic) IBOutlet UIImageView *cameraView;
+@property BOOL newMedia;
+
+@property (strong, nonatomic) IBOutlet UIImageView *cameraView;
 
 @end
 
@@ -26,6 +28,68 @@
 	// Dispose of any resources that can be recreated.
 }
 
+- (IBAction) useCamera:(id)sender
+{
+	if ([UIImagePickerController isSourceTypeAvailable:
+		 UIImagePickerControllerSourceTypeCamera])
+	{
+		UIImagePickerController *imagePicker = [[UIImagePickerController alloc] init];
+		imagePicker.delegate = self;
+		imagePicker.sourceType = UIImagePickerControllerSourceTypeCamera;
+		imagePicker.mediaTypes = @[(NSString *) kUTTypeImage];
+		imagePicker.allowsEditing = NO;
+		[self presentViewController:imagePicker animated:YES completion:nil];
+		_newMedia = YES;
+	}
+}
 
+- (IBAction) useCameraRoll:(id)sender
+{
+	if ([UIImagePickerController isSourceTypeAvailable:
+		 UIImagePickerControllerSourceTypeSavedPhotosAlbum])
+	{
+		UIImagePickerController *imagePicker = [[UIImagePickerController alloc] init];
+		imagePicker.delegate = self;
+		imagePicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+		imagePicker.mediaTypes = @[(NSString *) kUTTypeImage];
+		imagePicker.allowsEditing = NO;
+		[self presentViewController:imagePicker animated:YES completion:nil];
+		_newMedia = NO;
+	}
+}
+
+#pragma mark - UIImagePickerControllerDelegate
+
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+	NSString *mediaType = info[UIImagePickerControllerMediaType];
+	
+	[self dismissViewControllerAnimated:YES completion:nil];
+	
+	if ([mediaType isEqualToString:(NSString *)kUTTypeImage]) {
+		UIImage *image = info[UIImagePickerControllerOriginalImage];
+		
+		_cameraView.image = image;
+		if (_newMedia)
+			UIImageWriteToSavedPhotosAlbum(image, self, @selector(image:finishedSavingWithError:contextInfo:), nil);
+	}
+	else if ([mediaType isEqualToString:(NSString *)kUTTypeMovie])
+	{
+		// Code here to support video if enabled
+	}
+}
+
+-(void)image:(UIImage *)image finishedSavingWithError:(NSError *)error contextInfo:(void *)contextInfo
+{
+	if (error) {
+		UIAlertView *alert = [[UIAlertView alloc] initWithTitle: @"Save failed" message: @"Failed to save image" delegate: nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+		[alert show];
+	}
+}
+
+-(void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+	[self dismissViewControllerAnimated:YES completion:nil];
+}
 
 @end
